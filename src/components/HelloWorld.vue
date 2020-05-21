9bafd1303231145b8a158b1140c607f481767302
Nils Wisiol <mail@nils-wisiol.de>
2020-05-21 21:26:17 +0200

    fragenkatalog

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Hallo<br>Willkommen zum Flugzeugquiz.<br> Lass uns starten!!!!
+    </h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +36,41 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data: () => ({
+    questions: [
+      {
+        q: "Wer hat das Flugzeug erfunden?",
+        answers: [
+          "Leonardo Da Vinci",
+          "Wright Brothers",
+          "Michael Jackson",
+          "Nikola Tesla",
+        ],
+        correct: 1
+      },
+      {
+        q: "Welches Passagier Flugzeug ist das größte der Welt?",
+        answers: [
+          "Cirrus SR22",
+          "B747-400",
+          "A380-800",
+          "A350",
+        ],
+        correct: 2
+      },
+      {
+        q: "Welches Passagier Flugzeug ist das größte der Welt?",
+        answers: [
+          "Cirrus SR22",
+          "B747-400",
+          "A380-800",
+          "A350",
+        ],
+        correct: 2
+      },
+    ]
+  })
 }
 </script>
 
