37f41da85dc5b468124293e6364299c7b93c4cf7
Nils Wisiol <mail@nils-wisiol.de>
2020-05-21 23:15:39 +0200

    first version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,99 @@
 <template>
-  <div class="hello">
-    <h1>Hallo<br>Willkommen zum Flugzeugquiz.<br> Lass uns starten!!!!
-    </h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/df/TUIfly_737_01.jpg"
+          class="my-3"
+          contain
+          height="400"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Hallo und<br>Willkommen zum Flugzeugquiz.<br> Lass uns starten!!!!
+        </h1>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+
+        </h2>
+
+        <v-row justify="center">
+          <v-carousel
+                  :show-arrows="false"
+                  height="400"
+                  hide-delimiters
+                  v-model="q_index"
+          >
+            <v-carousel-item
+                    v-for="(question, i) in questions"
+                    :key="i"
+            >
+              <v-sheet
+                      :color="question.color"
+                      height="100%"
+              >
+                <v-row
+                        class="fill-height"
+                        align="center"
+                        justify="center"
+                >
+                  <div class="display-3">{{question.q}}</div>
+                  <v-btn rounded class="mx-2" @click="set_answer(0, question.correct)">{{question.answers[0]}}</v-btn>
+                  <v-btn rounded class="mx-2" @click="set_answer(1, question.correct)">{{question.answers[1]}}</v-btn>
+                  <v-btn rounded class="mx-2" @click="set_answer(2, question.correct)">{{question.answers[2]}}</v-btn>
+                  <v-btn rounded class="mx-2" @click="set_answer(3, question.correct)">{{question.answers[3]}}</v-btn>
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-snackbar
+            v-model="snackbar"
+            :timeout="0"
+    >
+      <p v-if="answer === correct">
+        Das ist richtig!!!!
+      </p>
+      <p v-if="answer !== correct">
+        Das war leider falsch.
+      </p>
+      <v-btn
+              color="pink"
+              text
+              @click="next_q()"
+      >
+        Nächste Frage
+      </v-btn>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data: () => ({
+    set_answer(a, c) {
+      this.answer = a;
+      this.correct = c;
+      this.snackbar = true;
+    },
+    next_q() {
+      this.snackbar = false;
+      this.q_index++;
+    },
+    snackbar: false,
+    answer: undefined,
+    correct: undefined,
+    q_index: 0,
     questions: [
       {
         q: "Wer hat das Flugzeug erfunden?",
@@ -47,7 +103,8 @@ export default {
           "Michael Jackson",
           "Nikola Tesla",
         ],
-        correct: 1
+        correct: 1,
+        color:"blue darken-4"
       },
       {
         q: "Welches Passagier Flugzeug ist das größte der Welt?",
@@ -57,37 +114,21 @@ export default {
           "A380-800",
           "A350",
         ],
-        correct: 2
+        correct: 2,
+        color: "teal"
       },
       {
-        q: "Welches Passagier Flugzeug ist das größte der Welt?",
+        q: "Welcher Flughafen ist der größte Flughafen der Welt?",
         answers: [
-          "Cirrus SR22",
-          "B747-400",
-          "A380-800",
-          "A350",
+          "Doha International Airport",
+          "Hartsfield Jackson International Airport",
+          "Frankfurt Airport",
+          "Berlin Tegel Airport",
         ],
-        correct: 2
+        correct:1,
+        color:"orange darken-4"
       },
     ]
   })
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
